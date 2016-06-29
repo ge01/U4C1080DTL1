b6a217862d1254192ca4c21aa4ac7452d7fb91b3
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-06-28 22:04:14 -0500

    Convert String to DateTime

---
+++ DateTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4C1080DTL1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // get first number
+            Console.Write("Enter the date:");
+
+            DateTime submittedDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine(submittedDate.ToString("D"));
+
+            // pause
+            Console.ReadLine();
+        }
+    }
+}
